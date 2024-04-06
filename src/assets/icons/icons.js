0e0421794cf4717734dca1543b0945cd286b570c
Anton <asiuraev@parseq.pro>
2024-04-06 21:47:45 +0500

    Fix conflict

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -3,6 +3,7 @@ import React from 'react'
 import IconsSVG from './icons.svg'
 
 function Icons({ name, color, size, className }) {
+  //TODO: [@asiuraev 03.04.2024] Проблема, что я иссользую size для ширины и высоты. Т.к. иконки могут быть не квадратными
   return (
     <svg className={`icon icon-${name} ${className}`} fill={color} stroke={color} width={size} height={size}>
       <use xlinkHref={`${IconsSVG}#icon-${name}`} />
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -12,6 +12,14 @@ export const limitString = (str: string, limit: number): string => {
   }
 }
 
+export function uuidv4 () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = Math.random() * 16 | 0
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
+
 export const withDelay = (callback: Function, delay: number) => {
   return setTimeout(callback, delay)
 }
